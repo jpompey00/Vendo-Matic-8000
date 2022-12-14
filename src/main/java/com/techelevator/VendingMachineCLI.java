586f2e088c1c323607aebbc67e489923ce44d336
Michael Kim <michaelkim120@gmail.com>
2022-12-14 18:56:40 -0500

    Added exit option and display items code.

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,11 +2,17 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
 
 	private Menu menu;
 
@@ -15,13 +21,28 @@ public class VendingMachineCLI {
 	}
 
 	public void run() {
+		// automatically restocked each time application is run
+
+
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
+				try (BufferedReader br = new BufferedReader(new FileReader("vendingmachine.csv"))) {
+					String line;
+					while ((line = br.readLine()) != null) {
+						System.out.println(line);
+					}
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
+				// exit
+				System.out.println("Thank you for using our vending machine.");
+				break;
 			}
 		}
 	}
