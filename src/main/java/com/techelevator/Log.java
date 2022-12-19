17888f553178403b43a0c604702282572aa212e3
Michael Kim <michaelkim120@gmail.com>
2022-12-18 22:23:56 -0500

    12/18 Hid sales report and added total sales

--- src/main/java/com/techelevator/Log.java
+++ src/main/java/com/techelevator/Log.java
@@ -1,6 +1,9 @@
 package com.techelevator;
 
 import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -37,21 +40,29 @@ public class Log {
     }
     public void getSalesReport(Product product) {
         DateTimeFormatter formatter
-                = DateTimeFormatter.ofPattern(
-                "ddMMyyyy HHmmss a");
+                = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HHmmss");
         LocalDateTime now = LocalDateTime.now();
         String dateTimeString = now.format(formatter);
-        try (PrintWriter dataOutput = new PrintWriter(new File(dateTimeString + ".txt"))) {
+        try (PrintWriter dataOutput = new PrintWriter(new File(dateTimeString + "-Sales-Report.txt"))) {
 
-            System.out.println(product.arrayList());
+            //LocalDateTime datetime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+            System.out.println(System.lineSeparator() + "Sales Report file created.");
+
+            //System.out.println(product.arrayList());
+
+            BigDecimal totalSales = new BigDecimal("0.00");
 
             for (int i = 0; i < product.arrayList().size(); i++) {
-                dataOutput.println(">"+ product.productArrayList.get(i).getName() + "|" + (5 - product.productArrayList.get(i).getStock()) + "\n");
-               // System.out.println(">" + product.productArrayList.get(i).getName() + "|" + (5 - product.productArrayList.get(i).getStock()) + "\n");
+                dataOutput.println(product.productArrayList.get(i).getName() + "|" + (5 - product.productArrayList.get(i).getStock()));
+                //System.out.println(">" + product.productArrayList.get(i).getName() + "|" + (5 - product.productArrayList.get(i).getStock()) + "\n");
+                totalSales = totalSales.add(BigDecimal.valueOf(product.productArrayList.get(i).getPrice()).setScale(2, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(5 - product.productArrayList.get(i).getStock())));
+                //System.out.println("TOTAL SALES: " + totalSales);
             }
-            System.out.println(product.productArrayList);
+            dataOutput.println(System.lineSeparator() + "TOTAL SALES: " + totalSales);
+            //System.out.println(product.productArrayList);
         } catch (FileNotFoundException e) {
-            System.err.println("Cannot open the file for writing.");}
+            System.err.println("Cannot open the file for writing.");
+        }
     }
 
     public String getLogList() {
